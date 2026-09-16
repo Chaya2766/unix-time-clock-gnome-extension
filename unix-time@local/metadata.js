0e3b30edebe3390778364285670f7a36585e3273
Chaya2766 <45914609+Chaya2766@users.noreply.github.com>
2026-09-16 12:13:34 +0000

    Delete unix-time@local/metadata.js
    Deleted a file I made by mistake which was never really needed

--- unix-time@local/metadata.js
+++
@@ -1,7 +0,0 @@
-{
-  "uuid": "unix-time@local",
-  "name": "Unix Time Display",
-  "description": "Shows current Unix timestamp and seconds since midnight",
-  "version": 1,
-  "shell-version": ["48"]
-}
